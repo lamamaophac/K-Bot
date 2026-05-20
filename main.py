98a83745c26706bf1ffa46c9888e702475cf3fd8
lamamaophac <lamamaophac@gmail.com>
2026-05-20 21:03:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3278,5 +3278,22 @@ async def bxhquan(ctx):
 # =========================
 # RUN
 # =========================
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=10000)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
 
 bot.run(TOKEN)
